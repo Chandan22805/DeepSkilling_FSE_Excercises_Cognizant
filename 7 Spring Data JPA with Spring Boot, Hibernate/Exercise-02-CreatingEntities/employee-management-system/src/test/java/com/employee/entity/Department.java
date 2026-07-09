c2957c743b90022edd1eb6b488adbe5157851e5e
Chandan <n.ranganath.chandan@gmail.com>
2026-07-10 00:56:10 +0530

    feat: create employee and department entities

---
+++ 7 Spring Data JPA with Spring Boot, Hibernate/Exercise-02-CreatingEntities/employee-management-system/src/test/java/com/employee/entity/Department.java
@@ -0,0 +1,15 @@
+package com.employee.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
